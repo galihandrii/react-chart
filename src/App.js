ad797904cc2edc8c4f3459b4e243cbcf46220b76
galihandrii <galihandrihasworo@gmail.com>
2023-02-02 22:46:35 +0700

    create chart with react-chart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import BarChart from './components/BarChart';
+import { useEffect, useState } from 'react';
+import { orderList } from './const/OrderList';
+
+
+//setup chart(important)
+import { CategoryScale } from 'chart.js';
+import { Chart } from 'chart.js/auto';
+Chart.register(CategoryScale);
+
 
 function App() {
+const [chartData, setChartData]=useState({
+  labels:[],
+  datasets: [
+    {
+      label: '',
+      data: [],
+      backgroundColor: [],
+    },
+  ],
+})
+
+
+useEffect(()=>{
+    //kalo pake api
+    //axios.get.then.catch
+    //then isinya setchartData
+
+  setChartData({
+    labels: orderList.map(data=>data.date),
+    datasets: [
+      {
+        label: 'Data penjualan blan agustus',
+        data: orderList.map((data) => data.totalOrder),
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+    ],
+  })
+},[])
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BarChart chartData={chartData}/>
     </div>
   );
 }
